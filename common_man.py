a65eb5559400e670885d494cdb3dde4428ebdef2
Isaiah Odhner <isaiahodhner@gmail.com>
2023-06-17 15:49:07 -0400

    Ignore other-language manpages by searching in man<N> folders
    Other languages have their folders beside the man<number> folders
    
    ChatGPT first tried to pass a glob to os.walk, but then fixed it.

--- common_man.py
+++ common_man.py
@@ -4,26 +4,29 @@ import re
 import gzip
 from collections import Counter
 import plotly.graph_objects as go
+import glob
 
 def search_manpages(manpages_dir):
     options = []
     file_paths = []
-    for root, _, files in os.walk(manpages_dir):
-        for file in files:
-            if file.endswith('.gz'):
-                manpage_path = os.path.join(root, file)
-                try:
-                    with gzip.open(manpage_path, 'rt', encoding='utf-8', errors='replace') as manpage_file:
-                        manpage_text = manpage_file.read()
-                        assert isinstance(manpage_text, str)
-                        program_options = re.findall(r'^(?:\s*)(?<!\\)-{1,2}[\w-]+(?<!-)', manpage_text, re.MULTILINE)
-                        options.extend(program_options)
-                        file_paths.extend([manpage_path] * len(program_options))
-                        print(f"Found {len(program_options)} options in {manpage_path}: {program_options!r}")
-                except IOError:
-                    print(f"Error: Failed to read manpage at {manpage_path}")
-                except UnicodeDecodeError:
-                    print(f"Error: Failed to decode manpage at {manpage_path}")
+    pattern = os.path.join(manpages_dir, 'man*')
+    for dirpath in glob.glob(pattern):
+        for root, _, filenames in os.walk(dirpath):
+            for filename in filenames:
+                if filename.endswith('.gz'):
+                    manpage_path = os.path.join(root, filename)
+                    try:
+                        with gzip.open(manpage_path, 'rt', encoding='utf-8', errors='replace') as manpage_file:
+                            manpage_text = manpage_file.read()
+                            assert isinstance(manpage_text, str)
+                            program_options = re.findall(r'^(?:\s*)(?<!\\)-{1,2}[\w-]+(?<!-)', manpage_text, re.MULTILINE)
+                            options.extend(program_options)
+                            file_paths.extend([manpage_path] * len(program_options))
+                            print(f"Found {len(program_options)} options in {manpage_path}: {program_options!r}")
+                    except IOError:
+                        print(f"Error: Failed to read manpage at {manpage_path}")
+                    except UnicodeDecodeError:
+                        print(f"Error: Failed to decode manpage at {manpage_path}")
     return options, file_paths
 
 def create_histogram(options, file_paths, top_n=10):
